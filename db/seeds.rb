f83d5a54deb5e5134b06536b7aab71a8b5622458
Baptiste Clair <baptisteclair@gmail.com>
2022-05-18 22:12:11 +0200

    simpleairbnb v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do
+  name = Faker::Address.community
+  description = Faker::Quote.matz
+  address = Faker::Address.full_address
+  price_per_night = rand(80..250)
+  number_of_guests = rand(1..10)
+  flat = Flat.new(name: name, description: description, address:address, price_per_night: price_per_night, number_of_guests: number_of_guests)
+  flat.save
+end
